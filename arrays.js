79ab13f9dd0c8eae9bfea5a99149349df6db307a
Aviad Sapir <aviadsapir@gmail.com>
2018-09-13 22:39:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, element){
+<<<<<<< HEAD
   return [element, ...arr];
 }
 function destructivelyAddElementToBeginningOfArray (array, element){
@@ -37,3 +38,39 @@ function removeElementFromEndOfArray(array){
 //var array = [1]
 //console.log(destructivelyAddElementToBeginningOfArray(array, 'foo'))
 //console.log(array)
+=======
+  var res = [element, ...arr]
+  return res
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  var res = [...array, element]
+  return res
+}
+
+function destructivelyAddElementToEndOfArray (array, element){
+  return array.push(element)
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length -1)
+}
+console.log(destructivelyAddElementToBeginningOfArray([1], 'foo'))
+>>>>>>> 9d183a3699439e1ed78a7ca95669564a525ddd32
